dc69b670485b10e990cef1a3b43db8beb5ef0d5c
Kim Stahl <kim.stahl@gmail.com>
2021-02-15 14:57:02 -0600

    created form handler

--- coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,4 +1,31 @@
 package org.launchcode.codingevents.controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping("events")
 public class EventController {
+
+    @GetMapping
+    public String displayAllEvents(Model model){
+        List events = new List();
+        events.add("Code With Pride");
+        events.add("Strange Loop");
+        events.add("Apple WWDC");
+        events.add("SpringOne Platform");
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+    @GetMapping("create")
+    public String renderCreateEventForm(){
+        return "events/create";
+    }
+
 }
